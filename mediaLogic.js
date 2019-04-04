c843160505edf8453d557e2586053eecabf75d3b
Akshay Gupta <akshay.gupta@entytle.com>
2019-04-04 17:11:57 +0530

    Replaced getUserMedia with mediaDevices.getUserMedia for video

--- mediaLogic.js
+++ mediaLogic.js
@@ -5,18 +5,30 @@ window.onload = function(){
 }
 
 // webcam video
-navigator.getMedia = (navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia);
+// navigator.getMedia = (navigator.mediaDevices.getUserMedia || navigator.mediaDevices.webkitGetUserMedia || navigator.mediaDevices.mozGetUserMedia);
 
-navigator.getMedia(
-  {video: true, audio: false},
+// navigator.getMedia(
+//   {video: true, audio: false},
+//
+//   function(mediaStream) {
+//     var video = document.getElementsByTagName('video')[0];
+//     video.src = window.URL.createObjectURL(mediaStream);
+//     video.play();
+//   },
+//
+//   function(error) {
+//     console.log(error);
+//   }
+// );
 
-  function(mediaStream) {
-    var video = document.getElementsByTagName('video')[0];
-    video.src = window.URL.createObjectURL(mediaStream);
+var constraints = { audio: false, video: true};
+
+navigator.mediaDevices.getUserMedia(constraints)
+.then(function(mediaStream) {
+  var video = document.querySelector('video');
+  video.srcObject = mediaStream;
+  video.onloadedmetadata = function(e) {
     video.play();
-  },
-
-  function(error) {
-    console.log(error);
-  }
-);
+  };
+})
+.catch(function(err) { console.log(err.name + ": " + err.message); });
